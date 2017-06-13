1fe24f17c562dae75e9f7a5dbed3c2fe604ea30a
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2017-06-13 12:17:48 +0700

    preparing logistration

---
+++ models/user.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+// User Schema
+var UserSchema = mongoose.Schema({
+    username: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password:{
+        type: String,
+        bcrypt: true
+    },
+    type:{
+        type: String
+    }
+});
+
+var User = module.exports = mongoose.model('User', userSchema)
+
+// SIngle User
+module.exports.getUserById = function(id, callback) {
+    User.findById(id, callback)
+}
+
+// User by Username
+module.exports.getUserByUsername = function(username, callback) {
+    var query = {username: username};
+    User.findOne(query, callback);
+}
+
+// Password
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+        // if(err) throw err;
+        callback(null, isMatch);
+    });
+}
+
+// Create Student
+module.exports.saveStudent = function(newUser, newStudent, callback) {
+    bcrypt.hash(newUser.passowrd, 10, function(err, hash) {
+        // if(err) throw err;
+        newUser.password = hash;
+        console.log('Student is being saved');
+        async.parallel([newUser.save, newStudent.save], callback);
+    });
+}
+
+// Create Instructor
+module.exports.saveInstructor = function(newUser, newInstructor, callback) {
+    bcrypt.hash(newUser.passowrd, 10, function(err, hash) {
+        // if(err) throw err;
+        newUser.password = hash;
+        console.log('Instructor is being saved');
+        async.parallel([newUser.save, newInstructor.save], callback);
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// User registration
+router.get('/register', function(req, res, next) {
+  res.render('users/register');
 });
 
 module.exports = router;
